f08553ddd46832c4e652f420d97601381e3d293e
chrisxesq <chrisesq@protonmail.com>
2021-02-03 01:04:40 -0500

    stack

--- week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -13,19 +13,27 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
-
+            opening_brackets_stack.append([i,next])    
+        elif next in ")]}":
+            if len(opening_brackets_stack)==0:
+                return i+1
+            else:
+                top = opening_brackets_stack.pop(-1)
+                if (next == ')' and top[1] == '(') or (next == ']' and top[1] == '[') or (next == '}' and top[1] == '{'):
+                    continue
+                else:
+                    return i + 1
+    if len(opening_brackets_stack)==0:
+        return 'Success'
+    
+    return opening_brackets_stack[0][0]+1
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
     main()
+
