ba557154d1425099df98f424c25570f5ddf10f85
chrisxesq <chrisesq@protonmail.com>
2021-02-18 06:14:34 -0500

    submitted

--- week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
+++ week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
@@ -12,14 +12,34 @@ def build_heap(data):
     # case gives a quadratic number of swaps.
     #
     # TODO: replace by a more efficient implementation
-    swaps = []
-    for i in range(len(data)):
-        for j in range(i + 1, len(data)):
-            if data[i] > data[j]:
-                swaps.append((i, j))
-                data[i], data[j] = data[j], data[i]
-    return swaps
+    # swaps = []
+    # for i in range(len(data)):
+    #     for j in range(i + 1, len(data)):
+    #         if data[i] > data[j]:
+    #             swaps.append((i, j))
+    #             data[i], data[j] = data[j], data[i]
+    # return swaps
 
+    def sift_down(node_index):
+        min_index = node_index
+        if 2*node_index + 1 <size:
+            l = 2*node_index + 1
+            if data[l]<data[min_index]:
+                min_index = l
+        if 2*node_index + 2 <size:
+            r = 2*node_index + 2
+            if data[r]<data[min_index]:
+                min_index = r 
+        if node_index != min_index:
+            swap_history.append((node_index, min_index))
+            data[node_index], data[min_index] = data[min_index], data[node_index]
+            return sift_down(min_index)
+    
+    swap_history = []
+    size = len(data)
+    for i in range(int(size/2),-1,-1):
+        sift_down(i)
+    return swap_history
 
 def main():
     n = int(input())
